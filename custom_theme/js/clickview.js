b7b4f6001dda8033b3a63195f2f87be2f0e92424
Edy <edytado@gmail.com>
2016-01-14 20:58:26 +0100

    responsivegallery refactored to clickview

--- custom_theme/js/clickview.js
+++ custom_theme/js/clickview.js
@@ -1,6 +1,6 @@
 /* Inspired on http://www.w3schools.com/css/css_image_gallery.asp (link: http://www.w3schools.com/css/tryit.asp?filename=trycss_image_gallery_responsive_js) */
 // Get the modal
-var modal = document.getElementById("responsivegallery_modal");
+var modal = document.getElementById("clickview_modal");
 function ImgResponsive_Close()
 {
 	if (modal)
@@ -10,20 +10,20 @@ if (modal)
 	modal.onclick = function() { ImgResponsive_Close(); }
 
 // Get the <span> element that closes the modal
-var span = document.getElementById("responsivegallery_close");
+var span = document.getElementById("clickview_close");
 
 // When the user clicks on <span> (x), close the modal
 if (span)
 	span.onclick = function()  { ImgResponsive_Close(); }
 
-var modalImg = document.getElementById("responsivegallery_content")
+var modalImg = document.getElementById("clickview_content")
 if (modalImg)
 	modalImg.onclick = function() { ImgResponsive_Close(); }
 
 // Get all images and insert the clicked image inside the modal
 // Get the content of the image description and insert it inside the modal image caption
 var images = document.getElementsByClassName("clickview");
-var captionText = document.getElementById("responsivegallery_caption");
+var captionText = document.getElementById("clickview_caption");
 for (var i = 0; i < images.length; i++) {
 	images[i].onclick = function(){
 		modal.style.display = "block";
